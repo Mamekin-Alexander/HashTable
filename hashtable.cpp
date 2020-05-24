4735f1ac2ebfe2ab9a18d72b8425894448378353
Alexander Mamekin <mamekin.al@gmail.com>
2020-05-24 22:09:28 +0300

    v1

---
+++ hashtable.cpp
@@ -0,0 +1,202 @@
+#include <vector>
+#include <iostream>
+#include "catch.hpp"
+#include "hash.h"
+#include "hashtable.h"
+#include <random>  
+#include <ctime>  
+#include <string>
+
+TEST_CASE ( "Hashtable works with strings", "[hashtable]" ) {
+    HashMap<std::string, int> h;
+    
+    REQUIRE(!h.Contains("one"));
+    REQUIRE(!h.Contains("two"));
+    
+    h["one"] = 2;
+    h["two"] = 1;
+    REQUIRE(h.Contains("one"));
+    REQUIRE(h.Contains("two"));
+    REQUIRE(!h.Contains("many"));
+    REQUIRE(!h.Contains("more"));
+    REQUIRE(!h.Contains("everything"));
+    
+    REQUIRE(h["one"] == 2);
+    REQUIRE(h["two"] == 1);
+    
+    h["one"] = 4;
+    h["two"] = 5;
+    
+    REQUIRE(h.Contains("one"));
+    REQUIRE(h.Contains("two"));
+    REQUIRE(!h.Contains("many"));
+    REQUIRE(!h.Contains("more"));
+    REQUIRE(!h.Contains("everything"));
+    
+    REQUIRE(h["one"] == 4);
+    REQUIRE(h["two"] == 5);
+}
+
+TEST_CASE ( "Hashtable supports element removal", "[hashtable]" ) {
+    HashMap<std::string, int> h;
+    
+    REQUIRE(!h.Contains("one"));
+    REQUIRE(!h.Contains("two"));
+    
+    h["one"] = 2;
+    h["two"] = 1;
+    REQUIRE(h.Contains("one"));
+    REQUIRE(h.Contains("two"));
+    REQUIRE(!h.Contains("many"));
+    REQUIRE(!h.Contains("more"));
+    REQUIRE(!h.Contains("everything"));
+    
+    REQUIRE(h["one"] == 2);
+    REQUIRE(h["two"] == 1);
+    
+    h.Remove("one");
+    
+    REQUIRE(!h.Contains("one"));
+    REQUIRE(h.Contains("two"));
+    REQUIRE(!h.Contains("many"));
+    REQUIRE(!h.Contains("more"));
+    REQUIRE(!h.Contains("everything"));
+    
+    REQUIRE(h["two"] == 1);
+}
+
+struct MyKey {
+    uint64_t a, b;
+    bool operator==(const MyKey& o) { return (a == o.a) && (b == o.b); }
+    
+    MyKey() = delete;
+    MyKey(MyKey&&) = default;
+    MyKey(uint64_t a_, uint64_t b_) : a(a_), b(b_) {}
+    MyKey(const MyKey&) = default;
+};
+
+template<>
+struct Hash<MyKey> {
+    Hash() = default;
+    
+    uint64_t operator() (const MyKey& v) const {
+        return v.a + v.b;
+    }
+};
+
+TEST_CASE ( "Hashtable works with custom Hash overloads", "[hashtable]" ) {
+    HashMap<MyKey, int> h;
+    
+    REQUIRE(!h.Contains({1, 2}));
+    REQUIRE(!h.Contains({2, 1}));
+    
+    h[{1, 2}] = 2;
+    h[{2, 1}] = 1;
+    
+    REQUIRE(h.Contains({1, 2}));
+    REQUIRE(h.Contains({2, 1}));
+    REQUIRE(!h.Contains({3, 0}));
+    REQUIRE(!h.Contains({0, 3}));
+    REQUIRE(!h.Contains({10, 20}));
+    
+    REQUIRE(h[{1, 2}] == 2);
+    REQUIRE(h[{2, 1}] == 1);
+    
+    h[{1, 2}] = 4;
+    h[{2, 1}] = 5;
+    
+    REQUIRE(h.Contains({1, 2}));
+    REQUIRE(h.Contains({2, 1}));
+    REQUIRE(!h.Contains({3, 0}));
+    REQUIRE(!h.Contains({0, 3}));
+    REQUIRE(!h.Contains({10, 20}));
+    
+    REQUIRE(h[{1, 2}] == 4);
+    REQUIRE(h[{2, 1}] == 5);
+}
+
+#ifdef HASH_IMPLEMENTED_FOR_INTEGRAL_TYPES
+
+#define MAKE_TEST(type) \
+{\
+    HashMap < type, int > h;\
+    REQUIRE(!h.Contains(1));\
+    REQUIRE(!h.Contains(2));\
+    h[1] = 2;\
+    h[2] = 1;\
+    REQUIRE(h.Contains(1));\
+    REQUIRE(h.Contains(2));\
+    REQUIRE(!h.Contains(3));\
+    REQUIRE(!h.Contains(4));\
+    REQUIRE(!h.Contains(1008));\
+    REQUIRE(h[1] == 2);\
+    REQUIRE(h[2] == 1);\
+    h[1] = 4;\
+    h[2] = 5;\
+    REQUIRE(h.Contains(1));\
+    REQUIRE(h.Contains(2));\
+    REQUIRE(!h.Contains(3));\
+    REQUIRE(!h.Contains(4));\
+    REQUIRE(!h.Contains(1008));\
+    REQUIRE(h[1] == 4);\
+    REQUIRE(h[2] == 5);\
+}
+
+TEST_CASE ( "Hashtable works with various int types", "[hashtable]" ) {
+    MAKE_TEST(int16_t)
+    MAKE_TEST(int32_t)
+    MAKE_TEST(int64_t)
+    
+    MAKE_TEST(uint16_t)
+    MAKE_TEST(uint32_t)
+    MAKE_TEST(uint64_t)
+    
+}
+#endif // HASH_IMPLEMENTED_FOR_INTEGRAL_TYPES
+
+#ifdef HASH_IMPLEMENTED_FOR_CLASSES_WITH_MAGIC_METHOD
+struct MyOtherKey {
+    uint64_t a, b;
+    bool operator==(const MyOtherKey& o) { return (a == o.a) && (b == o.b); }
+    
+    MyOtherKey() = delete;
+    MyOtherKey(MyOtherKey&&) = default;
+    MyOtherKey(uint64_t a_, uint64_t b_) : a(a_), b(b_) {}
+    MyOtherKey(const MyOtherKey&) = default;
+    
+    uint64_t GetHash() const {
+        return a + b;
+    }
+};
+
+TEST_CASE ( "Hashtable works with class GetHash method", "[hashtable]" ) {
+    HashMap<MyOtherKey, int> h;
+    
+    REQUIRE(!h.Contains({1, 2}));
+    REQUIRE(!h.Contains({2, 1}));
+    
+    h[{1, 2}] = 2;
+    h[{2, 1}] = 1;
+    
+    REQUIRE(h.Contains({1, 2}));
+    REQUIRE(h.Contains({2, 1}));
+    REQUIRE(!h.Contains({3, 0}));
+    REQUIRE(!h.Contains({0, 3}));
+    REQUIRE(!h.Contains({10, 20}));
+    
+    REQUIRE(h[{1, 2}] == 2);
+    REQUIRE(h[{2, 1}] == 1);
+    
+    h[{1, 2}] = 4;
+    h[{2, 1}] = 5;
+    
+    REQUIRE(h.Contains({1, 2}));
+    REQUIRE(h.Contains({2, 1}));
+    REQUIRE(!h.Contains({3, 0}));
+    REQUIRE(!h.Contains({0, 3}));
+    REQUIRE(!h.Contains({10, 20}));
+    
+    REQUIRE(h[{1, 2}] == 4);
+    REQUIRE(h[{2, 1}] == 5);
+}
+#endif // HASH_IMPLEMENTED_FOR_CLASSES_WITH_MAGIC_METHOD
---
+++ hashtable.h
@@ -0,0 +1,110 @@
+#include "hash.h"
+#include <list>
+#include <cstdlib>
+#include <ctime>
+#include <vector>
+#include <assert.h>
+template <class Key, class Value, class Hasher = Hash<Key>>
+class HashMap {
+	struct Link {
+		Key m_key;
+		Value m_value;
+	};
+	typedef std::list<Link> Chain;
+public:
+    HashMap() {
+		p = 100;
+		generate_a_b();
+		m_table.reserve(p);
+		for (int32_t i = 0; i < p; ++i)
+		{
+			m_table.push_back(Chain(i));
+		}
+	}
+    
+    // Feel free to skip these.
+    // See smart pointers lab instead, if don't know what they are.
+    HashMap(const HashMap<Key, Value>& other) = delete;
+    HashMap(HashMap<Key, Value>&& other) = delete;
+    HashMap& operator=(const HashMap<Key, Value>& other) = delete;
+    HashMap& operator=(HashMap<Key, Value>&& other) = delete;
+    
+    bool Contains(const Key& key) const { 
+		bool result = false;
+		int64_t index = get_index(key);
+		if (!(m_table.at(index).empty()))
+		{
+			typename Chain::const_iterator i = m_table.at(index).begin();
+			while (i != m_table.at(index).end())
+			{
+				if (i->m_key == key)
+				{
+					result = true;
+					break;
+				}
+				++i;
+			}
+		}
+		return result;
+	}
+    
+    void Remove(const Key& key) {
+		int64_t index = get_index(key);
+		if (!(m_table.at(index).empty()))
+		{
+			typename std::list<Link>::iterator i = m_table.at(index).begin();
+			while (i != m_table.at(index).end())
+			{
+				if (i->m_key == key)
+				{
+					m_table.at(index).erase(i);
+					break;
+				}
+				++i;
+			}
+		}
+	}
+    
+    Value& operator[](const Key& key) {
+		int64_t index = get_index(key);
+		if (Contains(key))
+		{
+			typename std::list<Link>::iterator i = m_table.at(index).begin();
+			while (i != m_table.at(index).end())
+			{
+				if (i->m_key == key)
+				{
+					return(i->m_value);
+				}
+				++i;
+			}
+		}
+		else
+		{
+			m_table.at(index).push_back(Link{ key, NULL });
+			return m_table.at(index).back().m_value;
+		}
+	}
+    
+    ~HashMap() {
+		a = NULL;
+		b = NULL;
+		p = NULL;
+		m_table.clear();
+    }
+    
+private:
+	void generate_a_b()
+	{
+		std::srand(static_cast<unsigned int>(time(0)));
+		a = std::rand() % p;
+		b = std::rand() % p;
+	}
+	int64_t get_index(const Key& key) const
+	{
+		return (a + b * m_hasher(key)) % p;
+	}
+	int64_t a, b, p;
+	std::vector<Chain> m_table;
+	Hasher m_hasher;
+};
